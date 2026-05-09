882477b161a9ae307714d6bd783708725691a83e
jho0314 <howawgagi0314@gmail.com>
2026-05-09 18:50:11 +0800

    Update Program.cs

--- VillarLibraryNowAPI/Program.cs
+++ VillarLibraryNowAPI/Program.cs
@@ -11,13 +11,10 @@ var app = builder.Build();
 //app.Urls.Add($"http://+:{port}");
     
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
 
